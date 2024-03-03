eabbfcfe0b2d9923098be652ea7a9e48a28eb4f5
Mikephie <131840160+Mikephie@users.noreply.github.com>
2024-03-03 21:25:08 +0800

    Update generate_image_json.py

--- .github/scripts/generate_image_json.py
+++ .github/scripts/generate_image_json.py
@@ -4,7 +4,7 @@ import json
 def generate_json():
     image_folder = 'icon'
     json_data = {
-        "name": "Sakura图标订阅",
+        "name": "Mikephie图标订阅",
         "description": "收集一些自己脚本用到的图标",
         "icons": []
     }
@@ -17,7 +17,7 @@ def generate_json():
             json_data["icons"].append(icon_data)
 
     # Set the output path relative to the repository root
-    output_path = os.path.join(os.getcwd(), 'sliverkiss.icons.json')
+    output_path = os.path.join(os.getcwd(), 'mikephie.icons.json')
 
     with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(json_data, json_file, ensure_ascii=False, indent=2)
